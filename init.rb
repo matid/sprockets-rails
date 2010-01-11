f684c7c9bbc6ad29d61760d27c82c7b9871c8943
Mateusz Drożdżyński <matid@matid.net>
2010-01-11 11:35:00 +0000

    Added a greceful fallback for init: this way Rails fall back to asking you to run 'rake gems:install' instead of aborting with 'no such file to load' error

--- init.rb
+++ init.rb
@@ -1,7 +1,11 @@
-require "sprockets"
-require "sprockets_helper"
-require "sprockets_application"
+begin
+  require "sprockets"
+  require "sprockets_helper"
+  require "sprockets_application"
 
-class ActionController::Base
-  helper :sprockets
-end
+  class ActionController::Base
+    helper :sprockets
+  end
+rescue LoadError
+  puts "Could not load sprockets. Please make sure the sprockets gem is installed an try again."
+end unless $gems_rake_task
